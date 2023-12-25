28369d0694334501937e8b9b03b16a69aeee75b4
dnaDeveloper2 <130073814+dnaDeveloper2@users.noreply.github.com>
2023-12-25 17:25:21 -0500

    Add files via upload

---
+++ build/precache-manifest.5c6999ca54502e34aab0ff18df81b927.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e40b8d1df333c416fbcd99757f53848a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c341e7e2d2ca5141999",
+    "url": "/static/css/main.48b0d621.chunk.css"
+  },
+  {
+    "revision": "dbf19fd96ea7ed36de7a",
+    "url": "/static/js/2.e1e0ccdf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.e1e0ccdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c341e7e2d2ca5141999",
+    "url": "/static/js/main.3cf7dea7.chunk.js"
+  },
+  {
+    "revision": "c8e5fd3b141acc9e29e7",
+    "url": "/static/js/runtime-main.e51d1157.js"
+  }
+]);
